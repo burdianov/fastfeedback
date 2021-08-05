0db7424bbdc20fea09fdac03ff79cdc34ed85793
Lilian Burdianov <lilian.burdianov@gmail.com>
2021-08-05 08:33:36 +0400

    implement chakra-ui

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,23 @@
-import Head from 'next/head';
-
 import { useAuth } from '../lib/auth';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const auth = useAuth();
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <main>
+        <h1>Fast Feeback</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Fast Feeback - Another deploy</h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+        <p>
+          <code>{auth.user ? auth.user.email : 'None'}</code>
         </p>
 
-        <button onClick={(e) => auth.signin()}>Sign In</button>
-        <div>{auth?.user?.email}</div>
-
-        {auth?.user && (
+        {auth.user ? (
           <button onClick={(e) => auth.signout()}>Sign Out</button>
+        ) : (
+          <button onClick={(e) => auth.signin()}>Sign In</button>
         )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
